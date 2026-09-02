db2afaf1a135f3f53b8aae1d8e6f50a1ea236609
Bryam Durazno <bryamdurazno@gmail.com>
2026-09-01 20:40:03 -0500

    Primer commit del proyecto CI/CD

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "Hola, este es mi primer proyecto CI/CD 🚀"
+
+if __name__ == "__main__":
+    app.run()
+
+
+    
